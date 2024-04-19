53c5e11e22aae2c7b0edf79958de1a56102aeb88
DrRon <DoctorRon64@users.noreply.github.com>
2024-04-19 19:20:19 +0200

    Functionality for adding programs and shaders added

--- coolgraphics/main.cpp
+++ coolgraphics/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-void processInput(GLFWwindow*& _window);
+//Forward Declaration
+void processInput(GLFWwindow*& window);
 int init(GLFWwindow*& window);
-void CreateTriangle();
+
+void CreateTriangle(GLuint &triangleVAO, int &size);
 void CreateShaders();
+void createProgram(GLuint& programID, const char* shaderFiles[]);
+
+//util
+void loadFile(const char* filename, char*& output);
+
+//programs ID
+GLuint coolProgram;
 
 int main()
 {
@@ -13,7 +23,9 @@ int main()
 	int setup = init(window);
 	if (setup != 0) return setup;
 
-	CreateTriangle();
+	GLuint triangleVAO;
+	int triangleSize;
+	CreateTriangle(triangleVAO, triangleSize);
 	CreateShaders();
 
 	//create viewport
@@ -21,28 +33,35 @@ int main()
 
 	//game render loop
 	while (!glfwWindowShouldClose(window)) {
-		//input handeling (TODO)
+		
+		//Input
+		processInput(window);
 
 		//rendering
-		//background color set & render!
-		glClearColor(1.0, 0.5, 0.3, 1.0);
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glfwSwapBuffers(window);
+		glUseProgram(coolProgram);
 
-		//events pollen
+		//what object do I want to draw
+		glBindVertexArray(triangleVAO);
+		glDrawArrays(GL_TRIANGLES, 0, triangleSize);
+
+		//Swap & Poll
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	return 0;
 }
 
-void processInput(GLFWwindow*& _window) {
-	if (glfwGetKey(_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(_window, true);
+void processInput(GLFWwindow*& window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
 	}
 }
 
-int init(GLFWwindow*& _window) {
+int init(GLFWwindow*& window) {
+
 	//Import GLFW major en minor versions
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -51,8 +70,8 @@ int init(GLFWwindow*& _window) {
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	//main window
-	_window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	if (_window == NULL)
+	window = glfwCreateWindow(800, 600, "My Cool Shaders", NULL, NULL);
+	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -60,9 +79,9 @@ int init(GLFWwindow*& _window) {
 	}
 
 	//set context
-	glfwMakeContextCurrent(_window);
+	glfwMakeContextCurrent(window);
 
-	//glad spul
+	//glad stuff
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
@@ -73,21 +92,114 @@ int init(GLFWwindow*& _window) {
 	return 0;
 }
 
-void CreateTriangle() {
+void CreateTriangle(GLuint& vao, int& size) {
+	//vertices of a triangle
 	float vertices[] = {
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
 		0.0f,  0.5f, 0.0f
 	};
 
-	unsigned int VAO;
-	glGenBuffers(1, &VAO);
-	glBindVertexArray(VAO);
+	//Vertex Array Object
+	glGenBuffers(1, &vao);
+	glBindVertexArray(vao);
 
-	unsigned int VBO;
+	//Vertex Buffer Object
+	GLuint VBO;
 	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW /*Willen we het updaten of static houden*/);
+	
+	//the position of an vertex is an vertex attribute
+	//tells OpenGL how to interpret vertex data for a specific attribute.
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	size = sizeof(vertices);
 }
 
 void CreateShaders() {
+	const char* shaderFiles[] = { "shaders/vertex.shader", "shaders/fragment.shader" };
+	createProgram(coolProgram, shaderFiles);
+}
 
+void createProgram(GLuint& programID, const char* shaderFiles[]) {
+
+	//Create a GL Program with a Vertex & Fragment Shader
+	char* vertexSource;
+	char* fragmentSource;
+
+	//load in from shader files
+	loadFile(shaderFiles[0], vertexSource);
+	loadFile(shaderFiles[1], fragmentSource);
+
+	GLuint vertexShaderID, fragmentShaderID;
+
+	vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShaderID, 1, &vertexSource, nullptr);
+	glCompileShader(vertexShaderID);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShaderID, 512, nullptr, infoLog);
+		std::cout << "o no error while compilng vertex shader\n" << infoLog << std::endl;
+	}
+
+	fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShaderID, 1, &fragmentSource, nullptr);
+	glCompileShader(fragmentShaderID);
+
+	glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShaderID, 512, nullptr, infoLog);
+		std::cout << "o no error while compilng vertex shader\n" << infoLog << std::endl;
+	}
+
+	programID = glCreateProgram();
+	glAttachShader(programID, vertexShaderID);
+	glAttachShader(programID, fragmentShaderID);
+	glLinkProgram(programID);
+
+	glGetShaderiv(programID, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(programID, 512, nullptr, infoLog);
+		std::cout << "o no link program something\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShaderID);
+	glDeleteShader(fragmentShaderID);
+
+	delete vertexSource;
+	delete fragmentSource;
+}
+
+void loadFile(const char* filename, char*& output) {
+	//open file
+	std::ifstream file(filename, std::ios::binary);
+
+	//if the file was opend good
+	if (file.is_open()) {
+		//get length of file
+		file.seekg(0, file.end);
+		std::streamoff fileLength = file.tellg();
+		file.seekg(0, file.beg);
+
+		//allocate memory for the char pointer
+		output = new char[fileLength + 1];
+
+		//read data as a block
+		file.read(output, fileLength);
+
+		//add null terminator to end of char pointer
+		output[fileLength] = '\0';
+
+		//close the file
+		file.close();
+	}
+	else {
+		//if the file could not open make the charpointer NULL
+		output = NULL;
+	}
 }
